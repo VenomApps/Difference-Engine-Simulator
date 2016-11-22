392b5a6b432a9373864b814214810439b5aaa1f4
Michael Davis <michael.e.davisjr@gmail.com>
2016-11-21 23:09:10 -0500

    Reconfigure 2.0

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -54,40 +54,100 @@ $(window).on('load',function(){
     //console.log(degree5, degree4, degree3, degree2, degree1, degree);
   }
 
+  function engine_alternate(difft, diff1t, diff2t, diff3t, diff4t, diff5t){
+    $('#tab').empty();
+    $('#equa').empty();
+    var diffp = difft;
+    var diff1p = diff1t;
+    var diff2p = diff2t;
+    var diff3p = diff3t;
+    var diff4p = diff4t;
+    var diff5p = diff5t;
+
+    var data1 = "<tr><th>X</th><th>Y</th><th>Y&prime;</th><th>Y&Prime;</th><th>Y&tprime;</th><th>Y&qprime;</th><th>Y&qprime;&prime;</th></tr>";
+    var count1 = $('#num').val();
+    data1 += `<tr> <td>0</td> <td>${diffp}</td> <td>${diff1p}</td> <td>${diff2p}</td> <td>${diff3p}</td> <td>${diff4p}</td> <td>${diff5p}</td> </tr>`;
+
+    for(var i=1; i<= count1; i++){
+      diffp = diffp+diff1p;
+      diff1p = diff1p+diff2p;
+      diff2p = diff2p + diff3p;
+      diff3p = diff3p+diff4p;
+      diff4p = diff4p+diff5p;
+      diff5p = diff5p;
+      data1 += `<tr> <td>${i}</td> <td>${diffp}</td> <td>${diff1p}</td> <td>${diff2p}</td> <td>${diff3p}</td> <td>${diff4p}</td> <td>${diff5p}</td> </tr>`;
+    }
+    $('#tab').append(data1);
+
+  }
+
   function load(){
+    if($('#dec').prop('checked') == true){
+      var deg5 = parseInt($('#deg5').val());
+      var deg4 = parseInt($('#deg4').val());
+      var deg3 = parseInt($('#deg3').val());
+      var deg2 = parseInt($('#deg2').val());
+      var deg1 = parseInt($('#deg1').val());
+      var deg = parseInt($('#deg').val());
 
-    var deg5 = parseInt($('#deg5').val());
-    var deg4 = parseInt($('#deg4').val());
-    var deg3 = parseInt($('#deg3').val());
-    var deg2 = parseInt($('#deg2').val());
-    var deg1 = parseInt($('#deg1').val());
-    var deg = parseInt($('#deg').val());
+      if(isNaN(deg5) == true){
+        deg5 = 0;
+      }
+      if(isNaN(deg4) == true) {
+        deg4 = 0;
+      }
 
-    if(isNaN(deg5) == true){
-      deg5 = 0;
+      if(isNaN(deg3) == true){
+        deg3 = 0;
+      }
+
+      if(isNaN(deg2) == true) {
+        deg2 = 0;
+      }
+
+      if(isNaN(deg1) == true) {
+        deg1 = 0;
+      }
+
+      if(isNaN(deg) == true) {
+        deg = 0;
+      }
+
+      console.log(deg5,deg4,deg3,deg2,deg1,deg);
+      engine(deg5,deg4,deg3,deg2,deg1,deg);
+    }else{
+      var diff5 = parseInt($('#diff5').val());
+      var diff4 = parseInt($('#diff4').val());
+      var diff3 = parseInt($('#diff3').val());
+      var diff2 = parseInt($('#diff2').val());
+      var diff1 = parseInt($('#diff1').val());
+      var diff = parseInt($('#diff').val());
+
+      if(isNaN(diff5) == true){
+        diff5 = 0;
+      }
+      if(isNaN(diff4) == true) {
+        diff4 = 0;
+      }
+
+      if(isNaN(diff3) == true){
+        diff3 = 0;
+      }
+
+      if(isNaN(diff2) == true) {
+        diff2 = 0;
+      }
+
+      if(isNaN(diff1) == true) {
+        diff1 = 0;
+      }
+
+      if(isNaN(diff) == true) {
+        diff = 0;
+      }
+      engine_alternate(diff,diff1,diff2,diff3,diff4,diff5);
     }
-    if(isNaN(deg4) == true) {
-      deg4 = 0;
-    }
 
-    if(isNaN(deg3) == true){
-      deg3 = 0;
-    }
-
-    if(isNaN(deg2) == true) {
-      deg2 = 0;
-    }
-
-    if(isNaN(deg1) == true) {
-      deg1 = 0;
-    }
-
-    if(isNaN(deg) == true) {
-      deg = 0;
-    }
-
-    console.log(deg5,deg4,deg3,deg2,deg1,deg);
-    engine(deg5,deg4,deg3,deg2,deg1,deg);
 
   }
 
